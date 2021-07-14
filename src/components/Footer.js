26d3d307cfa4e0ddfb080113c39658ac01c368d7
Alex Albaran <albaranlex@icloud.com>
2021-07-14 23:14:01 +0200

    added content to Footer.js component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyright &#169; 2021 ALEX ALBARAN | All Rights Reserved</p>
+    </div>
+  );
+}
+export default Footer;
